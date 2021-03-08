8c630583ebbcd10d82e3cd036dbdcbcfbd32731c
Robert Heath <robert.b.heathii@gmail.com>
2021-03-08 16:07:30 -0600

    Munchy switch

--- app/serializers/munchy_serializer.rb
+++ app/serializers/munchy_serializer.rb
@@ -2,5 +2,5 @@ class MunchySerializer
   include FastJsonapi::ObjectSerializer
   attributes :destination_city, :restaurant, :forecast, :travel_time
   attribute type: 'munchy'
-  set_id { 'null' } 
+  set_id { 'null' }
 end
