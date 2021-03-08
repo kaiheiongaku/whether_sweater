91587c3ec53595e787addecadb836b41bf25651c
Robert Heath <robert.b.heathii@gmail.com>
2021-03-08 15:42:43 -0600

    add destination city as an instance variable

--- app/poros/munchy.rb
+++ app/poros/munchy.rb
@@ -1,6 +1,7 @@
 class Munchy
-  attr_reader :travel_time, :forecast, :restaurant
-  def initialize(info)
+  attr_reader :travel_time, :forecast, :restaurant, :destination_city
+  def initialize(info, destination)
+    @destination_city = destination
     @travel_time = info[:travel_time]
     @forecast = info[:forecast]
     @restaurant = info[:restaurant]
