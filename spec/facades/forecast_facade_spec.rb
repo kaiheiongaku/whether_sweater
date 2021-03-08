3f8b14c53904d581e9a047c2afbbad9bd457b318
Robert Heath <robert.b.heathii@gmail.com>
2021-03-08 00:29:54 -0600

    Forecast facade

--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -1,19 +1,9 @@
 require 'rails_helper'
 
-describe 'forecast facade' do
-  describe 'happy path' do
-    it 'can get lat and long from maps' do
-      latlon = ForecastFacade.retrieve_latlon('mcallen,tx')
+describe ForecastFacade do
+  describe '' do
+    it '' do
 
-      expect(latlon).to eq({lattitude: 'xxx', longitude: 'yyy'})
-    end
-  end
-
-  describe 'sad path' do
-    it 'case does not matter' do
-      latlon = ForecastFacade.retrieve_latlon('McAlLeN,TX')
-
-      expect(latlon).to eq({lattitude: 'xxx', longitude: 'yyy'})
     end
   end
 end
