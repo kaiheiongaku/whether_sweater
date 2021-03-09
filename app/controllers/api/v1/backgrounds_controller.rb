94848a5a21488a4ea7bf01336627325f0c008afc
Robert Heath <robert.b.heathii@gmail.com>
2021-03-09 13:40:04 -0600

    Create routes and controller for images

---
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -0,0 +1,5 @@
+class Api::V1::BackgoundsController < ApplicationController
+  def index
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :forecast, only: :index
+      resources :backgrounds, only: :index
     end
   end
-  
+  # match '*unmatched_route', :to => 'application#raise_not_found!', :via => :all
 end
