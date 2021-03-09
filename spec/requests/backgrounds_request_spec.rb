c412cd52f3efdc892bb73d24a0fcd3b17670e925
Robert Heath <robert.b.heathii@gmail.com>
2021-03-09 15:48:14 -0600

    Update tests

---
+++ spec/requests/backgrounds_request_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe 'photo requests' do
+  describe 'happy path', :vcr do
+    it 'returns a photo for a city'  do
+      get '/api/v1/backgrounds?location=denver,co'
+
+      expect(response).to be_successful
+
+      parsed = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(parsed).to be_a(Hash)
+
+      expect(parsed).to have_key(:id)
+      expect(parsed[:id]).to eq('null')
+
+      expect(parsed).to have_key(:attributes)
+      expect(parsed[:attributes].keys).to eq([:location, :url, :credit])
+      expect(parsed[:attributes][:credit].keys).to eq([:author, :profile])
+
+      expect(parsed).to have_key(:type)
+      expect(parsed[:type]).to eq('background')
+    end
+  end
+
+  describe 'sad path' do
+
+  end
+end
