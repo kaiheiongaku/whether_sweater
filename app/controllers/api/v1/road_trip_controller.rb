b415e43e1c86798f266cb3cce7fbc18ec52ba4ac
Robert Heath <robert.b.heathii@gmail.com>
2021-03-10 12:27:43 -0600

    Create serializer and update controller

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
-
+    if User.find_by(api_key: params[:road_trip][:api_key])
+      render json: RoadtripSerializer.new(
+        RoadTripFacade.generate_roadtrip(params[:road_trip][:origin], params[:road_trip][:destination])
+      ), status: 201
+    else
+      render json: { error: 'Something went awry with the api key.'}, status: 401
+    end
   end
 end
