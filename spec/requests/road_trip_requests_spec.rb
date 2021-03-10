49a152d4f84776186bb6ef69d1f17236633ef260
Robert Heath <robert.b.heathii@gmail.com>
2021-03-10 12:29:32 -0600

    Update requests

--- spec/requests/road_trip_requests_spec.rb
+++ spec/requests/road_trip_requests_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'road trip requests' do
   describe 'post to road trip' do
     describe 'happy path' do
-      it 'returns a road trip' do
+      it 'returns a road trip', :vcr do
         user = User.create!(email: 'me@me.com', password: 'Turingrocks')
         headers = { 'ACCEPT' => 'application/json' }
         params = { 'road_trip': { 'origin': "mcallen,tx",
@@ -17,12 +17,12 @@ describe 'road trip requests' do
 
         road_trip = JSON.parse(response.body, symbolize_names: true)
 
-        expect(parsed).to be_a(Hash)
-        expect(parsed).to have_key(:data)
-        expect(parsed[:data].keys).to eq([:id, :type, :attributes])
-        expect(parsed[:data][:attributes].keys).to eq([:start_city, :end_city, :travel_time, :weather_at_eta])
-        expect(:data[:attributes][:weather_at_eta].keys).to eq(:temperature, :conditions)
-        expect(:data[:attributes][:weather_at_eta][:temperature]).to be_a(Float)
+        expect(road_trip).to be_a(Hash)
+        expect(road_trip).to have_key(:data)
+        expect(road_trip[:data].keys).to eq([:id, :type, :attributes])
+        expect(road_trip[:data][:attributes].keys).to eq([:start_city, :end_city, :travel_time, :weather_at_eta])
+        expect(road_trip[:data][:attributes][:weather_at_eta].keys).to eq([:temperature, :conditions])
+        expect(road_trip[:data][:attributes][:weather_at_eta][:temperature]).to be_a(Float)
       end
     end
   end
