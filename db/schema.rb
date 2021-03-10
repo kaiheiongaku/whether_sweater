840ac53aa321d87f652eeb78dc93e4ab07e1d7e6
Robert Heath <robert.b.heathii@gmail.com>
2021-03-09 19:14:46 -0600

    Update database

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_000224) do
+ActiveRecord::Schema.define(version: 2021_03_10_010415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
     t.string "password_digest"
+    t.string "email"
+    t.string "api_key"
   end
 
 end
