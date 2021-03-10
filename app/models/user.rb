1290b6d7640ac5098bd805235b9f68bc465b3050
Robert Heath <robert.b.heathii@gmail.com>
2021-03-09 19:15:06 -0600

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  validates :username, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
   validates_presence_of :password, require: true
+  validates_uniqueness_of :api_key
 
   has_secure_password
 end
