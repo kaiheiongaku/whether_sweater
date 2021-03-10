9984a59b49d4494d42e5d1243c17b5d8fa4b7f84
Robert Heath <robert.b.heathii@gmail.com>
2021-03-09 19:38:08 -0600

    Update user with api_key

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,14 @@ class User < ApplicationRecord
   validates_presence_of :password, require: true
   validates_uniqueness_of :api_key
 
+  before_create :set_api_key
+
+  def set_api_key
+    until self.api_key
+      self.api_key = SecureRandom.urlsafe_base64
+    end
+  end
+
+
   has_secure_password
 end
