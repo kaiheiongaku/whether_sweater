2a19eed446625036d2c5e5fd2563266e8123e851
Robert Heath <robert.b.heathii@gmail.com>
2021-03-10 12:00:25 -0600

    Create roadtrip poro

---
+++ app/poros/road_trip.rb
@@ -0,0 +1,27 @@
+class RoadTrip
+require 'rounding'
+  attr_reader :start_city, :end_city, :travel_time, :weather_at_eta
+
+  def initialize(origin, destination, info)
+    @start_city = origin
+    @end_city = destination
+    @travel_time = convert_time(info[:travel_time])
+    @weather_at_eta = weather(info[:hourly_weather_at_end])
+  end
+
+  def convert_time(time)
+    "#{time[0..1]} hours, #{time[2..3]} minutes"
+  end
+
+  def weather(hourly_weathers)
+    end_weather = {}
+    hourly = HourlyWeather.new(hourly_weathers[rounded_hours_until_arrival])
+    end_weather[:temperature] = hourly.temperature
+    end_weather[:conditions] = hourly.conditions
+    end_weather
+  end
+
+  def rounded_hours_until_arrival
+    (@travel_time[0..1].to_i * 60 + @travel_time[10..11].to_i) / 60
+  end
+end
---
+++ spec/poros/road_trip_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe RoadTrip do
+  it 'exists and has attributes' do
+    file = File.read('spec/fixtures/poros/hourly.json')
+
+    info = JSON.parse(file, symbolize_names: true)
+    roadtrip = RoadTrip.new('new york, ny', 'los angeles, ca', info)
+
+    expect(roadtrip).to be_a(RoadTrip)
+    expect(roadtrip.start_city).to eq('new york, ny')
+    expect(roadtrip.end_city).to eq('los angeles, ca')
+    expect(roadtrip.travel_time).to eq('40 hours, 34 minutes')
+
+    expect(roadtrip.weather_at_eta).to be_a(Hash)
+    expect(roadtrip.weather_at_eta[:temperature]).to eq(51.96)
+    expect(roadtrip.weather_at_eta[:conditions]).to eq('broken clouds')
+  end
+end
