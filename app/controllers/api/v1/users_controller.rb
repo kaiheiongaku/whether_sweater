4359327d14920f70dfece3bc8d1eb215c5749100
Robert Heath <robert.b.heathii@gmail.com>
2021-03-09 19:15:52 -0600

    Create user serializer and update controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,15 @@
 class Api::V1::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
+    if @user.save
+      render json: UsersSerializer.new(@user, status: 201)
+    else
+      render json: { error: 'I hate to be a confuser, but we could\'t create that user.'}, status: 400
+    end
   end
 
   private
   def user_params
-    binding.pry
-    params.require(:data).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password)
   end
 end
